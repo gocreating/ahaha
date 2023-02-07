c12856916c7ed0e7a32852404cb4116eeb2c7f75
gocreating <gocreating@gmail.com>
2023-02-07 21:18:59 +0800

    Add admin reminder

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,6 +3,17 @@ import PageLayout from '@/components/layouts/PageLayout'
 export default function Admin() {
   return (
     <PageLayout>
+      <details>
+        <summary>Hello, testers from Aha:</summary>
+        <p>
+          If you don't have permissions to see the admin dashboard, I've sent
+          you an email to request emails to grant to. Or if you have better
+          options, please let me know.
+          <br />
+          The working admin dashboard should seem like the following image:
+        </p>
+        <img src="/admin-screenshot.png" />
+      </details>
       <iframe
         src="https://lation.retool.com/apps/Ahaha"
         width="100%"
